0c3950d887a9ebdd983dbd6eb7f74958ea9918e1
indrajith <mailtoindrajith@gmail.com>
2023-08-24 11:26:05 +0530

    java calculator project created

---
+++ src/main/java/com/indrajith/javaCalculator/JavaCalc.java
@@ -0,0 +1,15 @@
+package com.indrajith.javaCalculator;
+
+public class JavaCalc {
+	//Method to add 2 Numbers
+	public int addNumbers(int numberOne, int numberTwo)
+	{
+		return numberOne + numberTwo;
+	}
+	//Method to subtract 2 Numbers
+		public int subtractNumbers(int numberOne, int numberTwo)
+		{
+			return numberOne - numberTwo;
+		}
+
+}
---
+++ src/test/java/com/indrajith/javaCalculator/JavaCalcJUnit.java
@@ -0,0 +1,21 @@
+package com.indrajith.javaCalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaCalcJUnit {
+
+	@Test
+	public void addTest() {
+		JavaCalc mycalc = new JavaCalc();
+		assertEquals(5,mycalc.addNumbers(5,5));
+	}
+	
+	@Test
+	public void subtractTest() {
+		JavaCalc mycalc = new JavaCalc();
+		assertEquals(5,mycalc.subtractNumbers(10,5));
+	}
+
+}
