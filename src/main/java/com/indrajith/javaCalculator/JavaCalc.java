d4b240bb358295b2deeb28f70275bbcb80e00886
indrajith <mailtoindrajith@gmail.com>
2023-08-24 12:19:02 +0530

    updated code due to code coverage failed

--- src/main/java/com/indrajith/javaCalculator/JavaCalc.java
+++ src/main/java/com/indrajith/javaCalculator/JavaCalc.java
@@ -4,12 +4,12 @@ public class JavaCalc {
 	//Method to add 2 Numbers
 	public int addNumbers(int numberOne, int numberTwo)
 	{
-		return numberOne + numberTwo;
+		return numberOne+numberTwo;
 	}
 	//Method to subtract 2 Numbers
 		public int subtractNumbers(int numberOne, int numberTwo)
 		{
-			return numberOne - numberTwo;
+			return numberOne-numberTwo;
 		}
 
 }
